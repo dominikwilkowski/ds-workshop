571034400341f01726784c1aacb97430d5a2d748
Dominik Wilkowski <hi@dominik-wilkowski.com>
2021-07-14 08:07:13 +1000

    added next friendly link component to markdown parser

--- docs/components/utils.js
+++ docs/components/utils.js
@@ -1,5 +1,15 @@
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
+
+function NextLink({ href, children, ...props }) {
+	return (
+		<Link href={href} passHref>
+			<a {...props}>{children}</a>
+		</Link>
+	);
+}
 
 export const mdxComponents = {
+	a: NextLink,
 	Button: dynamic(() => import('@ds-workshop/button').then((mod) => mod.Button)),
 };
