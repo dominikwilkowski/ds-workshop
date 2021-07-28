9a1d356738842559796a1876ddf5db7c806d6bb8
dominikwilkowski <dominikwilkowski@users.noreply.github.com>
2021-07-28 08:43:44 +0000

    deploy: a434d5407912d8f7ae9bbff595c832e3b8883f14

---
+++ _next/static/chunks/299.46fa62efc69ea6b1864b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[299],{3299:function(r,e,n){"use strict";n.r(e),n.d(e,{Box:function(){return s}});var t=n(7800);var u={name:"c6nbq9",styles:"background:var(--text);height:2rem;width:2rem;border-radius:3px"};function s(){return(0,t.tZ)("div",{css:u})}}}]);
