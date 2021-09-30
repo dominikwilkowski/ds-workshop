9653283b4e915abe7b0721039d545228e8e473d7
Dominik Wilkowski <hi@dominik-wilkowski.com>
2021-09-30 14:11:29 +1000

    fixed linting

--- packages/modal/src/Modal.js
+++ packages/modal/src/Modal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, Fragment } from 'react';
+import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import FocusLock from 'react-focus-lock';
 
